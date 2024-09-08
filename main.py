5bdd7247ad1809b48458a080bad116d1d1ca66a2
doquolo <doquolo123@outlook.com>
2024-09-09 00:06:32 +0700

    init

---
+++ main.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, render_template, jsonify
+from datetime import datetime, timedelta
+import json
+import dut
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        username = data['username']
+        password = data['password']
+        return {"status": dut.login(username, password)}        
+    else:
+        return render_template('login.html')
+
+@app.route('/getCal', methods=["GET"])
+def queryCal():
+    datecode = request.args.get("datecode")
+    return dut.getCal(datecode)
+
+@app.route('/getWeekInTerm', methods=["GET"])
+def getWeek():
+    
+    # get args
+    term = str(request.args.get("term"))
+
+    # load term data
+    with open("term.json", "r") as file:
+        term_data = json.loads(file.read())
+        file.close()
+
+    week_start_number = term_data[term]["weekstart"]
+    start_date = term_data[term]["start"]
+    end_date = term_data[term]["end"]
+
+
+    # Convert string dates to datetime objects
+    start_date = datetime.strptime(start_date, "%d/%m/%Y")
+    end_date = datetime.strptime(end_date, "%d/%m/%Y")
+    
+    current_start = start_date
+    current_week_number = week_start_number
+    
+    # Mapping of English weekdays to Vietnamese
+    vietnamese_weekdays = {
+        "Monday": "Thứ 2",
+        "Tuesday": "Thứ 3",
+        "Wednesday": "Thứ 4",
+        "Thursday": "Thứ 5",
+        "Friday": "Thứ 6",
+        "Saturday": "Thứ 7",
+        "Sunday": "Chủ Nhật"
+    }
+    
+    weeks = {}
+    while current_start <= end_date:
+        week_days = {}
+        for i in range(7):
+            current_day = current_start + timedelta(days=i)
+            if current_day > end_date:
+                break  # Stop if the current day exceeds the term's end date
+            # Get the Vietnamese weekday name
+            vietnamese_weekday = vietnamese_weekdays[current_day.strftime('%A')]
+            week_days[vietnamese_weekday] = current_day.strftime("%d/%m/%Y")
+        
+        weeks[current_week_number] = week_days
+        
+        current_start += timedelta(days=7)  # Move to the next Monday
+        current_week_number += 1  # Increment the week number
+    
+    return jsonify(weeks)
+
+@app.route("/getTimetable")
+def getTimetable():
+    with open("timetable.json", "r") as file:
+        data = json.loads(file.read())
+        file.close()
+    return jsonify(data)
+
+
+app.run('0.0.0.0', debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,129 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#loginbtn").addEventListener("click", () => {
+        const username = document.querySelector("#username").value;
+        const password = document.querySelector("#password").value;
+        if (username == "" || password == "") alert("Mã SV hoặc mật khẩu không được để trống!");
+        else {
+            fetch("/login", {
+                method: "POST",
+                body: JSON.stringify({ 'username': username, 'password': password})
+            })
+            .then(req => {
+                return req.json()
+            })
+            .then(req => {
+                if (req.status) {
+                    alert("Đăng nhập thành công!");
+                    document.querySelector("#loginpanel").style.display = 'none';
+                    document.querySelector("#mainpanel").style.display = 'flex';
+                } else {
+                    alert("Kiểm tra lại mssv và mật khẩu :(");
+                }
+            })
+        }
+    })
+    document.querySelector("#nampicker").value = new Date().getFullYear();
+    document.querySelector("#getData").addEventListener('click', () => {
+        const datecode = `${(document.querySelector('#nampicker').value) % 100}${document.querySelector('#hockipicker').value}`
+        fetch(`/getCal?datecode=${datecode}`)
+        .then(req => {return req.text()})
+        .then(res => {
+            document.querySelector("#data").innerHTML = res;
+
+            document.querySelector("#TTKB_GridInfo > tbody > tr:nth-child(1)").remove();
+            const child = document.createElement('th');
+            child.innerHTML = '<th class="GridHeaderCell" width="30">TT</th>';
+            document.querySelector("#TTKB_GridInfo > tbody > tr:nth-child(1)").prepend(child);
+
+            document.querySelector("#mainpanel").style.bottom = 'unset';
+
+            const schedule = tableToJSON("TTKB_GridInfo");
+            schedule.pop();
+            console.log(schedule);
+            parseToCal(schedule);
+        })
+    })
+
+    function tableToJSON(tableId) {
+        // Get the table element
+        const table = document.getElementById(tableId);
+        const headers = [];
+        const data = [];
+        
+        // Get headers from the first row of the table (assuming headers are in the first row)
+        const headerCells = table.querySelectorAll('tr.GridHeader th');
+        headerCells.forEach(header => headers.push(header.innerText.trim()));
+        
+        // Get rows from tbody
+        const rows = table.querySelectorAll('tbody tr.GridRow');
+        rows.forEach(row => {
+          const cells = row.querySelectorAll('td');
+          const rowData = {};
+          cells.forEach((cell, index) => {
+            // Avoid adding data if index exceeds header length
+            if (index < headers.length) {
+              rowData[headers[index]] = cell.innerText.trim();
+            }
+          });
+          data.push(rowData);
+        });
+      
+        return data
+    }
+
+    async function parseToCal(schedule) {
+        let weekInTerm;
+        await fetch(`/getWeekInTerm?term=${document.querySelector('#hockipicker').value}`)
+            .then(req => {return req.json()})
+            .then(res => weekInTerm = res);
+        let timetable;
+        await fetch(`/getTimetable`)
+            .then(req => {return req.json()})
+            .then(res => timetable = res);
+        var cal = ics();
+        for (let i in schedule) {
+                
+            const subjectName = `Môn: ${schedule[i]['Tên lớp học phần']}`;
+            const description = `GV: ${schedule[i]['Giảng viên']}`;
+            
+            const tkb_raw = (schedule[i]['Thời khóa biểu']).trim().split(";")
+            if (tkb_raw.length == 1) {
+                const tkb = (tkb_raw[0]).trim().split(",");
+                const weekday = tkb[0];
+                const period = (tkb[1]).trim().split("-");
+                const location = tkb[2];
+                const studyweek = (schedule[i]['Tuần học']).trim().split(";")
+                for (let i in studyweek) {
+                    const week = (studyweek[i]).trim().split("-");
+                    const weekStart = Number(week[0]); 
+                    const weekEnd = Number(week[1]);
+                    for (let j = weekStart; j < weekEnd+1; j++) {
+                        const timeStart = moment(`${weekInTerm[j][weekday]} ${timetable[period[0]]['start']}`, "DD/MM/YYYY HH:mm").toDate();
+                        const timeEnd = moment(`${weekInTerm[j][weekday]} ${timetable[period[1]]['end']}`, "DD/MM/YYYY HH:mm").toDate();
+                        cal.addEvent(subjectName, description, location, timeStart, timeEnd);
+                    }
+                }
+            } else {
+                for (let b in tkb_raw) {
+                    const tkb = (tkb_raw[b]).trim().split(",");
+                    const weekday = tkb[0];
+                    const period = (tkb[1]).trim().split("-");
+                    const location = tkb[2];
+                    const studyweek = (schedule[i]['Tuần học']).trim().split(";")
+                    for (let i in studyweek) {
+                        const week = (studyweek[i]).trim().split("-");
+                        const weekStart = Number(week[0]); 
+                        const weekEnd = Number(week[1]);
+                        for (let j = weekStart; j < weekEnd+1; j++) {
+                            const timeStart = moment(`${weekInTerm[j][weekday]} ${timetable[period[0]]['start']}`, "DD/MM/YYYY HH:mm").toDate();
+                            const timeEnd = moment(`${weekInTerm[j][weekday]} ${timetable[period[1]]['end']}`, "DD/MM/YYYY HH:mm").toDate();
+                            cal.addEvent(subjectName, description, location, timeStart, timeEnd);
+                        }
+                    }
+                }
+            }
+        }
+        console.log(cal.events());
+        cal.download();
+    }
+})
